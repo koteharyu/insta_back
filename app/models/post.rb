a6cce54e9431d3273014913f333e9a57df771236
haryu <hryau6@gmail.com>
2021-10-15 16:18:45 +0900

    add: association and seed data

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  valdiates :body, presence: true, length: { maximum: 150 }
+  validates :body, presence: true, length: { maximum: 150 }
 
   has_many_attached :image
 end

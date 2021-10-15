686b1ff96da0ebe995b6f95ad414560d7c92bbad
haryu <hryau6@gmail.com>
2021-10-15 15:54:38 +0900

    add: &. because faild to login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: session_params[:email])
-    if user.authenticate(session_params[:password])
+    if user&.authenticate(session_params[:password])
       token = Jwt::TokenProvider.call(user_id: user.id)
       render json: ActiveModelSerializers::SerializableResource.new(user, serializer: UserSerializer).as_json.deep_merge(user: { token: token })
     else

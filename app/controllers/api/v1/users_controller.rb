d4ae44e3fd74d6f72b6c33473958319ce1f086b9
haryu <hryau6@gmail.com>
2021-10-15 11:12:06 +0900

    add: user serializser

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: 200
+      render json: user, serializer: UserSerializer, status: 200
     else
       render json: user.errors, status: 400 # Bad Request
     end
